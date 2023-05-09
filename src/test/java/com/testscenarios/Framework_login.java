9e213518fda54601aa0ab5df01ceefebbfe00a59
umava <umava@192.168.86.23>
2023-05-09 17:56:12 -0400

    updated click method

--- src/test/java/com/testscenarios/Framework_login.java
+++ src/test/java/com/testscenarios/Framework_login.java
@@ -25,28 +25,18 @@ import java.util.Properties;
 	  	  driver.findElement(lc.JRI_login_email).sendKeys(p.getProperty("email"));
 	  	  driver.findElement(lc.JRI_login_password).sendKeys(p.getProperty("password"));
 	  	  driver.findElement(lc.JRI_login_captcha).sendKeys(p.getProperty("captcha"));
-	    }
+	    
 	  	  
 	  	  //Click on Secure signin button
 	  	  
 	  	  //driver.findElement(lc.JRI_login_signin).click();
-	  	public void clickByAnyLocator(By JRI_login_signin) throws Exception {	
-			WebElement element = driver.findElement(JRI_login_signin);
-			// Check your locator is displayed?
-			if (driver.findElements(JRI_login_signin).size() > 0) {
-				// Check your element is in enable state?
-				if (element.isEnabled()) {
-					// Click on Button/radiobutton/checkbox/Link...
-					element.click();
-				} else {
-					System.out.println("Given locator is not enable state on DOM(Current page***");
-				}
-			} else {
-				System.out.println("Given locator is not displayed on DOM(Current page***");
-	    }
+	  
+	  	clickByAnyLocator(lc.JRI_login_signin);
 	  
 
-	  	}
+			}
 	    }
+	    
+	 
   
 
